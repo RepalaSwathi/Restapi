20a627dedad87eee534d0756acf6a22485a37e80
RepalaSwathi <swathi.repala@dhanushinfotech.net>
2020-04-16 12:13:28 +0530

    project1

--- studentapi/models.py
+++ studentapi/models.py
@@ -9,10 +9,9 @@ class Student(models.Model):
 	email = models.CharField(max_length=40)
 	collegename = models.CharField(max_length=40)
 	age = models.IntegerField(blank=True, null=True)
-	#age = models.CharField(max_length=40)
+	
 	regnum = models.CharField(max_length=40,unique=True)
 	dob = models.CharField(max_length=40)
 	university = models.CharField(max_length=40)
 	username = models.CharField(max_length=40)
-	password = models.CharField(max_length=40)
-
+	password = models.CharField(max_length=40
